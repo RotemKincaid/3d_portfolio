72cb3209ca8fa16d9d806372ccbef2d22735cde2
Rotem Kincaid <rotema23@gmail.com>
2024-06-29 18:51:01 -0700

    Contact page done, started about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { skills } from '../constants'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='max-container'>
+      <h1 className='head-text'>Hello! I'm <span className='blue-gradient_text font-semibold drop-shadow'>Rotem</span></h1>
+
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>Software Engineer based in Arizona, specializing in Vue js and React with JavaScript and TypeScript. I love what I do!</p>
+      </div>
+
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>My Skills</h3>
+        <div className='mt-16 flex flex-wrap gap-12'>
+          {skills.map((skill, idx) => (
+            <div key={idx} className='block-container w-20 h-20'>
+              <div className='btn-back rounded-xl'/>
+              <div className='btn-front rounded-xl flex justify-center items-center'>
+                <img 
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className='w-1/2 h-1/2 object-contain'
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='py-16'>
+        <h3 className='subhead-text'>Work Experience</h3>
+        <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>I have worked with all sorts of companies, leveling up my skills and teaming up with smart people. Here's the rundown:</p>
+      </div>
+      </div>
+    </section>
   )
 }
 
